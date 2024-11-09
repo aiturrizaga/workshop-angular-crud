656c6a6a1a093631013886d6caa557bb319ccece
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2024-11-09 12:16:13 -0500

    Environments

--- src/app/core/services/person.service.ts
+++ src/app/core/services/person.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Person } from '../interfaces/person.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,23 @@ export class PersonService {
   private http = inject(HttpClient);
 
   findAll() {
-    return this.http.get<Person[]>('https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1/persons');
+    return this.http.get<Person[]>(`${environment.apiUrl}/persons`);
   }
 
   findById(id: string) {
-    return this.http.get<Person>('https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1/persons/' + id);
+    return this.http.get<Person>(`${environment.apiUrl}/persons/${id}`);
   }
 
   create(person: Person) {
-    return this.http.post<Person>('https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1/persons', person);
+    return this.http.post<Person>(`${environment.apiUrl}/persons`, person);
   }
 
   update(id: string, person: Person) {
-    return this.http.put<Person>('https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1/persons/' + id, person);
+    return this.http.put<Person>(`${environment.apiUrl}/persons/${id}`, person);
   }
 
   delete(id: string) {
-    return this.http.delete('https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1/persons/' + id);
+    return this.http.delete(`${environment.apiUrl}/persons/${id}`);
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: 'https://9lk3jtqj-8085.brs.devtunnels.ms/oraclecloud/v1'
+};
